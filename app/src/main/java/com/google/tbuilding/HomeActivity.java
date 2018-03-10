4bc1d76350a8d78297fe91570211c460777b2f03
Rainer Kent Renegado <renzkez@gmail.com>
2018-03-11 02:34:44 +0800

    Log-out function

--- app/src/main/java/com/google/tbuilding/HomeActivity.java
+++ app/src/main/java/com/google/tbuilding/HomeActivity.java
@@ -8,11 +8,30 @@ import android.content.*;
 
 public class HomeActivity extends Activity
 {
+	SharedPreferences sp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		sp = getSharedPreferences(getPackageName(), Activity.MODE_PRIVATE);
+		String userName = sp.getString("USER_NAME", null);
+		TextView name = findViewById(R.id.user_name);
+		name.setText(userName);
+		
+		TextView logout = findViewById(R.id.user_name);
+		logout.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				SharedPreferences.Editor editor = sp.edit();
+				editor.clear();
+				editor.apply();
+				startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+				finish();
+			}
+		});
+		
 		
 		Button newClaimButton = (Button) findViewById(R.id.newclaim);
 		newClaimButton.setOnClickListener(new OnClickListener(){
