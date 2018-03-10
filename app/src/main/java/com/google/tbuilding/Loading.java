a02faebe4cf5193049de49d12ba1bdb49669a6d7
Rainer Kent Renegado <renzkez@gmail.com>
2018-03-11 01:54:30 +0800

    Login function

--- app/src/main/java/com/google/tbuilding/Loading.java
+++ app/src/main/java/com/google/tbuilding/Loading.java
@@ -1,6 +1,7 @@
 package com.google.tbuilding;
 import android.os.*;
 import android.content.*;
+import android.content.SharedPreferences;
 import android.app.*;
 import android.widget.*;
 import android.view.animation.*;
@@ -13,27 +14,33 @@ public class Loading extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
 
-		ImageView img = (ImageView)findViewById(R.id.imgs);
-        TextView text = (TextView)findViewById(R.id.txt);
-		Animation fade = AnimationUtils.loadAnimation(this,R.anim.transition);
+		ImageView img = (ImageView) findViewById(R.id.imgs);
+        TextView text = (TextView) findViewById(R.id.txt);
+		Animation fade = AnimationUtils.loadAnimation(this, R.anim.transition);
 		img.startAnimation(fade);
-		Animation fadeout = AnimationUtils.loadAnimation(this,R.anim.transitionout);
+		Animation fadeout = AnimationUtils.loadAnimation(this, R.anim.transitionout);
 		text.startAnimation(fadeout);
-		final Intent  a = new Intent(this,SignInActivity.class);
-		Thread timer = new Thread(){
-				public void run(){
-						try{
-								sleep(5000);
-							}catch(InterruptedException e){
-								e.printStackTrace();
-							}
-						finally{
-						   startActivity(a);
-								finish();
 
-							}
-					}
-			};
+		SharedPreferences sp = getSharedPreferences(getPackageName(), Activity.MODE_PRIVATE);
+		final Intent a;
+		if (sp.getString("USER_UID", null) != null) {
+			a = new Intent(this, HomeActivity.class);
+		} else {
+			a = new Intent(this, SignInActivity.class);
+		}
+
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(3000);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					startActivity(a);
+					finish();
+				}
+			}
+		};
 		timer.start();
-}
+	}
 }
--- app/src/main/java/com/google/tbuilding/SignInActivity.java
+++ app/src/main/java/com/google/tbuilding/SignInActivity.java
@@ -1,10 +1,13 @@
 package com.google.tbuilding;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignInActivity extends AppCompatActivity {
     Button blog;
@@ -22,7 +30,7 @@ public class SignInActivity extends AppCompatActivity {
     EditText etpass;
     View progressView;
 
-    FirebaseAuth auth;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,10 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         setReferences();
 
-        auth = FirebaseAuth.getInstance();
+        final SharedPreferences.Editor sp = getSharedPreferences(getPackageName(), Activity.MODE_PRIVATE).edit();
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         blog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,17 +63,7 @@ public class SignInActivity extends AppCompatActivity {
 
                 progressView.setVisibility(View.VISIBLE);
 
-                // if (etuser.getText().toString().equals("admin") &&
-                //        etpass.getText().toString().equals("admin")) {
-                //    Intent loginIntent = new Intent(getApplicationContext(), HomeActivity.class);
-                //    startActivity(loginIntent);
-                //    Toast.makeText(getApplicationContext(),
-                //            "Logging in .......", Toast.LENGTH_SHORT).show();
-
-                // } else {
-                //    Toast.makeText(SignInActivity.this, "Wrong Credentials", Toast.LENGTH_SHORT).show();
-                // }
-                auth.signInWithEmailAndPassword(email, password)
+                firebaseAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
@@ -77,6 +78,21 @@ public class SignInActivity extends AppCompatActivity {
                             } else {
                                 Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
                                 startActivity(intent);
+                                final String userUid = firebaseAuth.getCurrentUser().getUid();
+                                final DatabaseReference currentUserDB = databaseReference.child(userUid);
+                                currentUserDB.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        String userFullName = dataSnapshot.child("firstname").getValue().toString() + " " + dataSnapshot.child("lastname").getValue().toString();
+                                        sp.putString("USER_UID" , userUid);
+                                        sp.putString("USER_NAME" , dataSnapshot.child("firstname").getValue().toString());
+                                        sp.apply();
+                                    }
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                        Log.e("SignInActivity", "onCancelled", databaseError.toException());
+                                    }
+                                });
                                 finish();
                             }
                         }
